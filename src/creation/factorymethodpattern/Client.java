1388e73db5287f72453302852dddd4c68ea7a7dd
Dimo-2562 <chosehoon2@naver.com>
2025-11-30 22:56:52 +0900

    feat: Client 코드 최적화

--- src/creation/factorymethodpattern/Client.java
+++ src/creation/factorymethodpattern/Client.java
@@ -1,11 +1,17 @@
 package creation.factorymethodpattern;
 
+import creation.factorymethodpattern.creator.BlackShipFactory;
+import creation.factorymethodpattern.creator.ShipFactory;
 import creation.factorymethodpattern.creator.WhiteShipFactory;
-import creation.factorymethodpattern.product.Ship;
 
 public class Client {
+    private void print(ShipFactory factory, String name) {
+        System.out.println(factory.orderShip(name));
+    }
+
     public static void main(String[] args) {
-        Ship whiteship = new WhiteShipFactory().orderShip("whiteShip");
-        System.out.println(whiteship);
+        Client c = new Client();
+        c.print(new WhiteShipFactory(), "White Ship");
+        c.print(new BlackShipFactory(), "Black Ship");
     }
 }
