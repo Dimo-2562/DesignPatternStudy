a0ebf2dd023325e72b3b6dab0da6754064c91eaa
Dimo-2562 <chosehoon2@naver.com>
2025-12-08 20:05:13 +0900

    refactor: 서비스는 보통 public 접근자를 사용하므로 변경

--- src/structure/adapterpattern/Service.java
+++ src/structure/adapterpattern/Service.java
@@ -1,7 +1,7 @@
 package structure.adapterpattern;
 
 public class Service {
-    void specificMethod(int specialData){
+    public void specificMethod(int specialData){
         System.out.println("기존 서비스 기능 호출 + " + specialData);
     }
 }
